e451b7cde2b473af376d95cf85ca057aeb442e2f
Tafelglotzer <nico.rickenbach@gmail.com>
2017-06-11 16:09:43 +0200

    Fix format

--- src/Formatter/Furnidata/XmlFormatter.php
+++ src/Formatter/Furnidata/XmlFormatter.php
@@ -9,28 +9,28 @@ class XmlFormatter implements FormatterInterface
     public function format(array $input)
     {
         return [sprintf('
-            <furnitype id="%d" classname="%s">
-                <revision>%d</revision>
-                <defaultdir>0</defaultdir>
-                <xdim>%d</xdim>
-                <ydim>%d</ydim>
-                <partcolors/>
-                <name>%s</name>
-                <description>%s</description>
-                <adurl/>
-                <offerid>-1</offerid>
-                <buyout>0</buyout>
-                <rentofferid>-1</rentofferid>
-                <rentbuyout>0</rentbuyout>
-                <bc>1</bc>
-                <excludeddynamic>0</excludeddynamic>
-                <customparams/>
-                <specialtype>1</specialtype>
-                <canstandon>0</canstandon>
-                <cansiton>0</cansiton>
-                <canlayon>0</canlayon>
-                <furniline/>
-            </furnitype>
+<furnitype id="%d" classname="%s">
+    <revision>%d</revision>
+    <defaultdir>0</defaultdir>
+    <xdim>%d</xdim>
+    <ydim>%d</ydim>
+    <partcolors/>
+    <name>%s</name>
+    <description>%s</description>
+    <adurl/>
+    <offerid>-1</offerid>
+    <buyout>0</buyout>
+    <rentofferid>-1</rentofferid>
+    <rentbuyout>0</rentbuyout>
+    <bc>1</bc>
+    <excludeddynamic>0</excludeddynamic>
+    <customparams/>
+    <specialtype>1</specialtype>
+    <canstandon>0</canstandon>
+    <cansiton>0</cansiton>
+    <canlayon>0</canlayon>
+    <furniline/>
+</furnitype>
         ',
             $input['id'],
             $input['swf'],
